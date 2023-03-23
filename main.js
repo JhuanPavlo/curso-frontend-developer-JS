27086f94d7ce867d538daabf294676d037b28a84
Pablo Triana <jhuanpablotriana@gmail.com>
2023-03-22 21:02:21 -0500

    Adicion de carrito

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
 }
 function toggleMobileMenu () {
     mobileMenu.classList.toggle('inactive');
+    aside.classList.add('inactive');
+    
+}
+function toggleCarritoAside () {
+    aside.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
 }
